03c0e4cbf9ec05be2557f24483deef20b9ebc743
jorge.soasti <jorgesoasti@gmail.com>
2017-09-28 16:01:52 -0500

    Initial commit

---
+++ app/src/main/java/com/jorgesoasti/navegacionhaciaatras/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jorgesoasti.navegacionhaciaatras;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void irSegundaActividad(View v){
+        Intent intent = new Intent(this, SegundaActivity.class);
+        startActivity(intent);
+    }
+}
